356180176c91d0b43fb938f51b0914638a5bdab2
Ignat Insarov <kindaro@gmail.com>
2018-08-26 18:55:55 +0700

    Add (?) operator.

--- Experiment.hs
+++ Experiment.hs
@@ -7,10 +7,27 @@ module Experiment
   where
 
 class Microtemplate micro
+  where
+    (?) :: String -> micro -> micro
+    infixl 2 ?
 
-instance Show show => Microtemplate (show -> String)
+instance {-# overlaps #-} Show show => Microtemplate (show -> String)
+  where
+    s ? m = \x -> s ++ m x
+
+-- |
+-- λ "One: " ? show $ 1
+-- "One: 1"
 
 instance (Show show, Microtemplate micro) => Microtemplate (show -> micro)
+  where
+    s ? m = \x -> s ? m x
+
+-- |
+-- λ ("Two: " ? (show .) . (+)) 1 1
+-- "Two: 2"
+-- λ ("Three: " ? \x y z -> show $ x + y + z) 1 1 1
+-- "Three: 3"
 
 class Ellipsis left right out
   where
